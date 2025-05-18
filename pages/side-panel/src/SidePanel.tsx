ef0aec2cf7d6728096c7e0fe7cf618d6654989d9
snghnl <viselacity@gmail.com>
2025-05-18 13:51:08 +0900

    add: add skeleton for side panel && add mui

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -3,25 +3,28 @@ import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
 import { ToggleButton } from '@extension/ui';
 import { t } from '@extension/i18n';
+import { Divider, Typography } from '@mui/material';
+import { ChartCustom } from '@src/features/chart-customization/ChartCustom';
+import { IndexingList } from '@src/features/chart-index/IndexingList';
+import { DataSource } from '@src/features/data-source/DataSource';
 
 const SidePanel = () => {
-  const theme = useStorage(exampleThemeStorage);
-  const isLight = theme === 'light';
-  const logo = isLight ? 'side-panel/logo_vertical.svg' : 'side-panel/logo_vertical_dark.svg';
-  const goGithubSite = () =>
-    chrome.tabs.create({ url: 'https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite' });
-
   return (
-    <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'}`}>
-      <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
-        <button onClick={goGithubSite}>
-          <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
-        </button>
-        <p>
-          Edit <code>pages/side-panel/src/SidePanel.tsx</code>
-        </p>
-        <ToggleButton onClick={exampleThemeStorage.toggle}>{t('toggleTheme')}</ToggleButton>
-      </header>
+    <div className="mt-4">
+      <Typography variant="h1" sx={{ fontSize: '2rem', fontWeight: 'bold' }}>
+        Indexing List
+      </Typography>
+      <IndexingList />
+      <Divider />
+      <Typography variant="h1" sx={{ fontSize: '2rem', fontWeight: 'bold' }}>
+        Chart Customization
+      </Typography>
+      <ChartCustom />
+      <Divider />
+      <Typography variant="h1" sx={{ fontSize: '2rem', fontWeight: 'bold' }}>
+        Data Source
+      </Typography>
+      <DataSource />
     </div>
   );
 };
